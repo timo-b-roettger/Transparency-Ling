a1dc56a2a52d33ad6092692c2d665dba3448fe9a
Timo Roettger <timoroettger@hf-163-223.uio.no>
2021-04-01 15:55:52 +0200

    first commit

---
+++ scripts/00_doodling.R
@@ -0,0 +1,41 @@
+
+############
+### Info ###
+############
+#
+## Authors:     Timo Roettger (timo.b.roettger @ gmail.com)
+#
+## Project:     Transparency in Linguistics
+#          
+## Description: Exploring the scopus output
+#
+## Date edited: 2021-04-01
+#
+#-------------------------------------------------------------------------------
+
+
+
+## Set-up & preprocessing--------------------------------------------------------
+
+# libraries
+library(rstudioapi)
+library(tidyverse)
+
+# getting the path of your current open file
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path))
+setwd("../data/")
+
+# load in data
+df <- tibble(read.csv("scopus_dummy.csv"))
+
+# check journals
+unique(df$Source.title)
+
+# get number of hits per journal
+df_hits <- df %>%
+  group_by(Source.title) %>% 
+  summarise(sum = n()) %>% 
+  arrange(desc(sum))
+
+df_hits
