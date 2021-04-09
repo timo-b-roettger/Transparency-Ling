ab1eb40193158c65621beabce293da22b1375374
Timo Roettger <timoroettger@hf-163-223.uio.no>
2021-04-09 12:51:13 +0200

    extracted data of 2008-2009 and 2018-2019, wrote random sampling script

--- scripts/00_doodling.R
+++ scripts/00_doodling.R
@@ -27,16 +27,49 @@ setwd(dirname(current_path))
 setwd("../data/")
 
 # load in data
-df <- tibble(read.csv("scopus_dummy.csv"))
+df2008 <- tibble(read.csv("scopus_2008.csv"))
+df2009 <- tibble(read.csv("scopus_2009.csv"))
+df2018 <- tibble(read.csv("scopus_2018.csv"))
+df2019 <- tibble(read.csv("scopus_2019.csv"))
+
+# merge dfs
+df <- rbind(df2008,df2009,df2018,df2019)
 
 # check journals
 unique(df$Source.title)
 
 # get number of hits per journal
 df_hits <- df %>%
-  group_by(Source.title) %>% 
+  group_by(Source.title, Year) %>% 
   summarise(sum = n()) %>% 
   arrange(desc(sum))
 
 print(df_hits, n = 20)
+
+# set random seed
+set.seed(999)
+
+# create sample vector (1 = in sample, 0 = out of sample)
+df$sample = 0
+
+# subset data into pre-crisis and post-crisis
+df$subset <- ifelse(df$Year %in% c(2008, 2009), "pre-crisis", "post-crisis")
+
+df_pre <- df %>% 
+  filter(subset == "pre-crisis")
+
+df_post <- df %>% 
+  filter(subset == "post-crisis")
+
+# randomly sample 250 papers for each subset (plus 20 for pilot)
+df_pre[sample(nrow(df_pre), 260), ]$sample  <- 1:260
+df_post[sample(nrow(df_post), 260), ]$sample  <- 1:260
+
+# merge
+df_final <- full_join(df_pre, df_post)
+
+# indicate pilot vs. critical papers
+df_final$data_type <- ifelse(df_final$sample >= 251, "pilot", 
+                             ifelse(df_final$sample == 0, "NA", "critical"))
+
 
